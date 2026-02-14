f56d55075bed9c229fe45d3974bc2950d7f005ea
ashish singh <ashishsingh094562@gmail.com>
2026-02-14 23:06:00 +0530

    update

--- model_loader.py
+++ model_loader.py
@@ -5,7 +5,7 @@ from huggingface_hub import hf_hub_download
 from config import REPO_ID, MODEL_FILE, DISEASE_JSON, LABELS_JSON
 
 
-@st.cache_resource
+
 def download_assets():
     model_path = hf_hub_download(REPO_ID, MODEL_FILE)
     disease_path = hf_hub_download(REPO_ID, DISEASE_JSON)
@@ -13,14 +13,13 @@ def download_assets():
     return model_path, disease_path, labels_path
 
 
-@st.cache_resource
+
 def load_model(model_path):
     interpreter = tf.lite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
     return interpreter
 
 
-@st.cache_resource
 def load_json(path):
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
